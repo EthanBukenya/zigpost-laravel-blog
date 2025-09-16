87aa6e780fddc2ab9ef52ec199660068303bcd48
EthanBukenya <ethancylb@gmail.com>
2025-09-16 15:48:38 +0300

    initial commit

---
+++ resources/views/includes/adsense-banner.blade.php
@@ -0,0 +1,11 @@
+<div class="ad-container my-4">
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-XXXXXXXXXXXXXXXXX"
+         data-ad-slot="1234567810"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
---
+++ resources/views/includes/adsense-sidebar.blade.php
@@ -0,0 +1,11 @@
+<div class="ad-container mb-4">
+    <ins class="adsbygoogle"
+         style="display:block"
+         data-ad-client="ca-pub-XXXXXXXXXXXXXXXXX"
+         data-ad-slot="0987654321"
+         data-ad-format="auto"
+         data-full-width-responsive="true"></ins>
+    <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
